604c762d5ccdb90eaaf652957f50ee5add02568b
Ashadowmonarch <forcoding39@gmail.com>
2025-07-20 00:45:19 -0400

    add instructions

---
+++ main.js
@@ -0,0 +1,20 @@
+/* main.js */
+
+// BIOS
+// 512 bytes on disk
+// store at 0xfc00
+
+// magic string
+//
+
+// 1. Initialize stack
+// sp
+// 16 bit register
+// copy value to AX register
+// copy from AX to SP
+
+// 2. Print the message
+
+let ctors;
+
+ctors = {};
