73d60f48af889198ec233769aae6fde4acc5eb9c
Ashadowmonarch <forcoding39@gmail.com>
2025-07-20 10:38:57 -0400

    fix comments

--- tmp.asm
+++ tmp.asm
@@ -1,13 +1,16 @@
 bits 16
+org 0xfc00        ; Set origin address to BIOS load address (for emulation/debug)
 
+;mov ax, 0x7c00     ; Load stack address into AX
+mov sp, ax         ; Initialize the stack pointer
 
-nop
-nop
-dd 0x0
-mov bx, 0xaabb
-mov ax, 0xaabb
-mov cx, 0xaabb
-mov sp, ax
-dd 0x0
-nop
-nop
+; nop
+; nop
+; dd 0x0
+; mov bx, 0xaabb
+; mov ax, 0xaabb
+; mov cx, 0xaabb
+; mov sp, ax
+; dd 0x0
+; nop
+; nop
